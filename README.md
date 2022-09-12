af9805f5813e4f1e073307a2b047f8088cf895c9
Antonio Carlos de Lisboa <antoniolisboa@alunos.utfpr.edu.br>
2022-09-12 10:32:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mineirim-language
---
+++ Tokens.py
@@ -0,0 +1,42 @@
+class Tokens:
+    '''List of tokens'''
+
+    # Reserved word tokens
+    TK_RW_MAIN = 0
+    TK_RW_FUNCTION = 1
+    TK_RW_PRINT = 2
+    TK_RW_SCANF = 3
+    TK_RW_WHILE = 4
+    TK_RW_RETURN = 5
+    TK_RW_REAL = 6
+    TK_RW_INTEGER = 7
+    TK_RW_STRING = 8
+    TK_RW_IF = 9
+    TK_RW_ELIF = 10
+    TK_RW_ELSE = 11
+    TK_RW_INDETINFIER = 12
+
+    # Tokens
+    TK_INTEGER = 13
+    TK_REAL = 14
+    TK_STRING = 15
+    TK_OP = 16
+    TK_CP = 17
+    TK_OK = 18
+    TK_CK = 19
+    TK_COMMA = 20
+    TK_MATH_ADD = 21
+    TK_MATH_SUB = 22
+    TK_MATH_MUL = 23
+    TK_MATH_DIV = 24
+    TK_LOGIC_OR = 25
+    TK_LOGIC_AND = 26
+    TK_LOGIC_NOT = 27
+    TK_LOGIC_DIF = 28
+    TK_LOGIC_LT = 29
+    TK_LOGIC_LTE = 30
+    TK_LOGIC_GT = 31
+    TK_LOGIC_GTE = 32
+    TK_ASSIGNMENT = 33
+    TK_LOGIC_EQ = 34
+    TK_END = 35
